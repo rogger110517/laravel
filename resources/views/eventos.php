1d161f5cc959feae0b478102424ead16c010034b
David Balbuena Soriano <rogger@ideperu.net>
2021-08-19 04:08:08 -0500

    primer cambio

---
+++ resources/views/eventos.php
@@ -0,0 +1,21 @@
+@include('template/header')
+<main class="main" style="height: 100vh;">
+    <section class="desktop">
+                <div class="desktop_information w-100" style="height: 15vh;">
+                    <div class="desktop_information_content w-100 text-end">
+                        <a href="" class="desktop_information_content_user"><i class="fas fa-user-circle font-size-15 pr-05"></i>Usuario</a>
+                    </div>
+                </div>
+    </section>
+    <section>
+        <div style="background-color: #000000; width: 100%; height: 7.5vh">
+        </div>
+        <div style="height: 70vh">
+            <iframe src="https://innovaciondigital.pe/" style="width: 100%; height: 100%"></iframe>
+        </div>
+        <div style="background-color: #000000; width: 100%; height: 7.5vh"  >
+
+        </div>
+    </section>
+</main>
+@include('template/footer')
---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    </footer>
+    <script src="https://kit.fontawesome.com/4ca23cb350.js" crossorigin="anonymous"></script>
+    <script src="header.js"></script>
+</body>
+</html>
